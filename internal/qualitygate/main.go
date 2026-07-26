20c511e38122aed2e2b1ffcb4c71605650128e2e
Steven Buglione <stevenbuglione1@gmail.com>
2026-07-26 11:18:12 -0400

    Add deterministic typed mail messages

--- internal/qualitygate/main.go
+++ internal/qualitygate/main.go
@@ -378,6 +378,7 @@ func fuzz(ctx context.Context, root string) error {
 		{"./config", "FuzzResolveScalars"},
 		{"./web", "FuzzDecodeJSON"},
 		{"./httpclient", "FuzzResolveReference"},
+		{"./mail", "FuzzNewMessage"},
 		{"./session", "FuzzManagerLoad"},
 		{"./starter", "FuzzParseManifest"},
 		{"./view", "FuzzRendererEscaping"},
